9568070b81d815b17fff6e080aecf169c127276b
Guilherme <g.satilio@gmail.com>
2024-04-29 16:00:02 -0300

    Add project files.

---
+++ OO_EstanteLivros/Livro.cs
@@ -0,0 +1,63 @@
+﻿internal class Livro
+{
+    public string titulo;
+    public string[] autores = new string[3];
+    public DateOnly dtlancamento;
+    public string editora;
+    public int edicao;
+    public int isbn;
+    public int paginas;
+
+    public Livro()
+    {
+
+    }
+    public void DefinirTitulo(string dados)
+    {
+        this.titulo = dados;
+    }
+    public void DefinirAutores(int autores, string dados)
+    {
+        this.autores[autores] = dados;
+    }
+    public void DefinirDtLancamento(DateOnly dados)
+    {
+        this.dtlancamento = dados;
+    }
+    public void DefinirEditora(string dados)
+    {
+        this.editora = dados;
+    }
+    public void DefinirEdicao(int dados)
+    {
+        this.edicao = dados;
+    }
+    public void DefinirISBN(int dados)
+    {
+        this.isbn = dados;
+    }
+    public void DefinirPaginas(int dados)
+    {
+        this.paginas = dados;
+    }
+    public string InformarLivro()
+    {
+        string Livro = "";
+        Livro += $"Título: {this.titulo}\n";
+        Livro += $"Dt.Lançamento: {this.dtlancamento}\n";
+        Livro += $"Autor(es): ";
+        for (int i = 0; i < 3; i++)
+        {
+            if (this.autores[i] != null)
+            {
+                Livro += $"{this.autores[i]}. ";
+            }
+        }
+        Livro += $"Editora: {this.editora}\n";
+        Livro += $"Edição: {this.edicao}\n";
+        Livro += $"ISBN: {this.isbn}\n";
+        Livro += $"{this.paginas} páginas";
+
+        return Livro;
+    }
+}
---
+++ OO_EstanteLivros/Program.cs
@@ -0,0 +1,96 @@
+﻿string[] Estante = new string[10];
+int QtdeLivros = 0, EscolherLivro = 0, contLivro = 0;
+Livro meuLivro = new Livro();
+
+Livro GerarObjetoLivro(int nrolivro)
+{
+    int autores = 0;
+    Livro objLivro = new Livro();
+
+    Console.WriteLine($"Informe o título do seu {contLivro + 1} livro:");
+    objLivro.DefinirTitulo(Console.ReadLine());
+
+    while (autores <= 0)
+    {
+        Console.WriteLine("Quantos autores possui o seu livro?");
+        autores = int.Parse(Console.ReadLine());
+    }
+    for (int i = 0; i < autores; i++)
+    {
+        Console.WriteLine($"Infome o nome do {i + 1}o Autor:");
+        objLivro.DefinirAutores(i, Console.ReadLine());
+    }
+
+    Console.WriteLine("Informe a data de lançamento do livro:");
+    objLivro.DefinirDtLancamento(DateOnly.Parse(Console.ReadLine()));
+
+    Console.WriteLine("Informe a Editora do seu livro:");
+    objLivro.DefinirEditora(Console.ReadLine());
+
+    Console.WriteLine("Informe a edição do seu livro:");
+    objLivro.DefinirEdicao(int.Parse(Console.ReadLine()));
+
+    Console.WriteLine("Informe o código ISBN do seu livro:");
+    objLivro.DefinirISBN(int.Parse(Console.ReadLine()));
+
+    Console.WriteLine("Informe a quantidade de páginas do seu livro:");
+    objLivro.DefinirPaginas(int.Parse(Console.ReadLine()));
+
+    return objLivro;
+}
+
+void InserirLivro()
+{
+    while (QtdeLivros <= 0)
+    {
+        Console.WriteLine("Informe a quantidade de livros que deseja inserir em sua Estante: ");
+        QtdeLivros = int.Parse(Console.ReadLine());
+    }
+    for (int i = 0; i < QtdeLivros; i++)
+    {
+        meuLivro = GerarObjetoLivro(QtdeLivros);
+        Estante[i] = meuLivro.InformarLivro();
+        contLivro++;
+    }
+}
+
+void ImprimirEstante()
+{
+    int posicao;
+    Console.WriteLine("Escolha o seu livro informando o número de 1 a 10.\nCaso deseja ver todos os livros, digite 0.");
+    posicao = int.Parse(Console.ReadLine());
+    switch (posicao)
+    {
+        case 0:
+            for (int i = 0; i < 10; i++)
+            {
+                if (Estante[i] != null)
+                {
+                    Console.WriteLine(Estante[i]);
+                    Console.WriteLine();
+                }
+            }
+            break;
+        default:
+            while (posicao > 0 && posicao < 10)
+            {
+                if (Estante[posicao - 1] == null)
+                {
+                    Console.WriteLine("Essa prateleira não possui livros!");
+                    posicao = -1;
+                }
+                else
+                {
+                    Console.WriteLine(Estante[posicao - 1]);
+                }
+            }
+            break;
+    }
+}
+
+InserirLivro();
+while (true)
+{
+    ImprimirEstante();
+}
+
